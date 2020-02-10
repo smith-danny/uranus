f21e7204c5179774835a33210088099a8e778c12
Anthony Angelo <anthony.angelo@servicenow.com>
2020-02-10 10:37:08 -0500

    Update

--- backend.tf
+++
@@ -1,9 +0,0 @@
-# Save Terraform State to S3 Bucket
-terraform {
-  backend "s3" {
-    bucket = "uranus-terraform-backend"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
-  }
-}
-
--- locals.tf
+++
@@ -1,5 +0,0 @@
-# Define local variables
-locals {
-  # Define the common tags for all resources
-  common_tags = "${var.tags}"
-}
