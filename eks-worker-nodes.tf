3e54e2a74c8db159486c61b0a3caec6238c9178a
Anthony Angelo <anthonyangelo@yahoo.com>
2021-01-19 18:26:33 -0500

    Update

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -25,23 +25,23 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "kubernetes-node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = "aws_iam_role.kubernetes-node.name"
+  role       = aws_iam_role.kubernetes-node.name
 }
 
 resource "aws_iam_role_policy_attachment" "kubernetes-node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = "aws_iam_role.kubernetes-node.name"
+  role       = aws_iam_role.kubernetes-node.name
 }
 
 resource "aws_iam_role_policy_attachment" "kubernetes-node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = "aws_iam_role.kubernetes-node.name"
+  role       = aws_iam_role.kubernetes-node.name
 }
 
 resource "aws_eks_node_group" "kubernetes" {
-  cluster_name    = "aws_eks_cluster.kubernetes.name"
+  cluster_name    = aws_eks_cluster.kubernetes.name
   node_group_name = "kubernetes"
-  node_role_arn   = "aws_iam_role.kubernetes-node.arn"
+  node_role_arn   = aws_iam_role.kubernetes-node.arn
   subnet_ids      = aws_subnet.kubernetes[*].id
 
   scaling_config {
--- variables.tf
+++ variables.tf
@@ -8,6 +8,12 @@ variable "cluster-name" {
 }
 
 variable "kubernetes_version" {
-  default = "1.17"
+  default = "1.18"
   type    = string
+}
+
+variable "kubeconfig_name" {
+  description = "Override the default name used for items kubeconfig."
+  type        = string
+  default     = ""
 }
